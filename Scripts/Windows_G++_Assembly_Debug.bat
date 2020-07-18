1396981cca81a419ca962ede30c66bf1d8a288c1
dominisieli <dominicsieli@gmail.com>
2020-07-18 01:49:03 -0700

    Refactored compile scripts

--- Scripts/Windows_G++_Assembly_Debug.bat
+++ Scripts/Windows_G++_Assembly_Debug.bat
@@ -2,6 +2,11 @@ cd ..
 
 mkdir Assembly
 
+@echo off
+echo "#pragma once" > Source\Configuration.h
+echo "#define WINDOWS" >> Source\Configuration.h
+echo "#define DEBUG_MODE" >> Source\Configuration.h
+
 for %%I in (.) do set project=%%~nxI
 set file=%project%_%~n0
 
