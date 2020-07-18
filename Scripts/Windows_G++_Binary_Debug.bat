1437a7ae7d6b714b34d50377f47f39c9f9842080
Ashley Olson <dominicsieli@gmail.com>
2020-07-18 13:01:00 -0700

    Updated compiler scripts

--- Scripts/Windows_G++_Binary_Debug.bat
+++ Scripts/Windows_G++_Binary_Debug.bat
@@ -5,7 +5,7 @@ mkdir Binaries
 @echo off
 echo "#pragma once" > Source\Configuration.h
 echo "#define WINDOWS" >> Source\Configuration.h
-echo "#define DEBUG_MODE" >> Source\Configuration.h
+echo "#define DEBUG" >> Source\Configuration.h
 
 for %%I in (.) do set project=%%~nxI
 set file=%project%_%~n0
