e5fa50473f0904bc6d4f4f3d3f259c5a458c57f7
dominisieli <dominicsieli@gmail.com>
2020-08-08 14:51:51 -0700

    Added Array Class

---
+++ Source/Array.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstring>
+
+template <typename T, unsigned long long S>
+class Array
+{
+private:
+	T data[S];
+
+public:
+	constexpr unsigned long long Size() const { return S; }
+
+	T& operator[](unsigned long long index) { return data[index]; }
+	const T& operator[](unsigned long long index) const { return data[index]; }
+
+	T* Data() { return data; }
+	const T* Data() const { return data; }
+};
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include "Array.h"
+
 int main()
 {
-	std::cout << "Success!" << '\n';
+	Array<int, 10> array;
+
+	for(unsigned long long i = 0; i < array.Size(); i++)
+	{
+		array[i] = i;
+	}
+
+	for(unsigned long long i = 0; i < array.Size(); i++)
+	{
+		std::cout << array[i] << '\n';
+	}
+
 	std::cin.get();
 }
